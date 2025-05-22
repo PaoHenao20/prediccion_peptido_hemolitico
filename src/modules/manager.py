0bb7838ad42d1b8e925db5e5b7bf6df424107b21
Paola <paolaahenao7@gmail.com>
2025-05-21 22:37:20 -0500

    feat: agregar funcion limpieza

--- src/modules/manager.py
+++ src/modules/manager.py
@@ -1,5 +1,5 @@
 from extract import download_file_from_url
-from transform import concat_csv_sin_duplicados, get_smiles
+from transform import clean_df, concat_csv_sin_duplicados, get_smiles
 
 # HemoPI2 data - information link: https://webs.iiitd.edu.in/raghava/hemopi2/download.html
 # ToxinPred3 - information link:  https://webs.iiitd.edu.in/raghava/toxinpred3/download.php
@@ -27,6 +27,15 @@ resultado = concat_csv_sin_duplicados(MAIN_FOLDER,
                                         pattern="*.csv",
                                         output_path=salida)
 print(f"Concatenados {len(resultado)} filas únicas.")
+
+
+# Limpia y guarda
+df_limpio = clean_df(
+    resultado,
+    output_csv="data/processed/all_data_clean.csv"
+)
+
+print(df_limpio)
 
 # seq_input = "PYK-K-W-P-R-P-D-A-P-I-P-P"
 # smiles = get_smiles(seq_input)
